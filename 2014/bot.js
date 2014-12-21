c1d16bd8960a99091728330e07bd6959e635c0fd
RnbwNoise <rnbwnoise@yandex.ru>
2014-12-21 00:43:16 -0500

    Added functions for debugging, renamed IIC.userListeners to IIC._userListeners.

--- 2014/bot.js
+++ 2014/bot.js
@@ -1,10 +1,10 @@
-IIC.userListeners = {};
+IIC._userListeners = {};
 
 // Sets a handler (function(data)) for a given event. Returns the handler's "identifier".
 IIC.addEventListener = function(event, listener) {
     // We aren't calling user listeners for this event: start doing so.
-    if(!this.userListeners[event]) {
-        this.userListeners[event] = [];
+    if(!this._userListeners[event]) {
+        this._userListeners[event] = [];
         
         var defaultListener = events[event];
         events[event] = function(data) {
@@ -12,26 +12,26 @@ IIC.addEventListener = function(event, listener) {
             defaultListener(data);
             
             // And then all of the user-defined ones.
-            for(var i = 0; i < this.userListeners[event].length; i++) {
-                if(this.userListeners[event][i])
-                    this.userListeners[event][i](data);
+            for(var i = 0; i < this._userListeners[event].length; i++) {
+                if(this._userListeners[event][i])
+                    this._userListeners[event][i](data);
             }
         }.bind(this);
     }
     
     // Add user's listener.
-    return [ event, this.userListeners[event].push(listener) - 1 ];
+    return [ event, this._userListeners[event].push(listener) - 1 ];
 };
 
 // Removes an event handler with a given identifier. Returns true if the listener was successfully removed.
 IIC.removeEventListener = function(listenerId) {
     // Make sure the id is valid and the listener exists.
     if(!listenerId || !listenerId[0] || (typeof listenerId[1]) === 'undefined'
-       || !this.userListeners[listenerId[0]] || !this.userListeners[listenerId[0]][listenerId[1]])
+       || !this._userListeners[listenerId[0]] || !this._userListeners[listenerId[0]][listenerId[1]])
         return false;
 
     // Delete the listener.
-    this.userListeners[listenerId[0]][listenerId[1]] = null;
+    this._userListeners[listenerId[0]][listenerId[1]] = null;
     return true;
 };
 
@@ -126,6 +126,52 @@ IIC.onGhost = function(listener) {
         if(data.button === 'right')
             listener(data.id, data.x, data.y);
     });
+};
+
+
+IIC._DEBUG_POINT_SIZE = 4;
+IIC._debugElements = [];
+
+// Adds a div for debugging marks.
+IIC._addDebugDiv = function(x, y) {
+    var element = document.createElement('div');
+    
+    element.style.position = 'absolute';
+    element.style.left = x + 'px';
+    element.style.top = y + 'px';
+    
+    document.body.appendChild(element);
+    this._debugElements.push(element);
+    
+    return element;
+};
+
+// Draws a point at a given location for debugging purposes.
+IIC.debugPoint = function(x, y, color) {
+    var pointElement = this._addDebugDiv(x - this._DEBUG_POINT_SIZE / 2, y - this._DEBUG_POINT_SIZE / 2);
+    
+    pointElement.style.width = this._DEBUG_POINT_SIZE + 'px';
+    pointElement.style.height = this._DEBUG_POINT_SIZE + 'px';
+    
+    pointElement.style.backgroundColor = color;
+};
+
+// Draws a string at a given location for debugging purposes.
+IIC.debugText = function(x, y, string, color) {
+    var textElement = this._addDebugDiv(x, y);
+    textElement.innerText = string;
+    
+    textElement.style.fontFamily = 'sans-serif';
+    
+    if(color)
+        textElement.style.color = color;
+};
+
+// Erases all the debugging marks.
+IIC.debugErase = function() {
+    for(var i = 0; i < this._debugElements.length; i++)
+        this._debugElements[i].parentElement.removeChild(this._debugElements[i]);
+    this._debugElements = [];
 };
 
 
